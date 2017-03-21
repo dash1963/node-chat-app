81734adb496d1771142f0eaa7dfa4b047690f878
Sergio Rueda <sergio4234@gmail.com>
2017-03-20 23:00:39 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,14 +30,15 @@
 
      });
 
-
+     var messageTextBox = jQuery("[name=message]");
      jQuery("#message-form").on("submit", function(e) {
          e.preventDefault();
 
          socket.emit("createMessage", {
            from: "User",
-           text: jQuery("[name=message]").val()
+           text: messageTextBox.val()
          }, function(){
+            messageTextBox.val("")
          });
      });
 
@@ -47,12 +48,16 @@
        return alert("Geolocation not suppoerted by your browser");
     }
 
+  locationButton.attr("disabled", 'disabled').text("Sending location...");
+
     navigator.geolocation.getCurrentPosition(function(position){
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     }, function() {
+      locationButton.removeAttr('disabled').text('Send location');
       alert("Unable to fetch location.")
     })
   })
