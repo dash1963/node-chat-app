844ad4ecf13965ddc55e2b85264707f9d9357e74
Sergio Rueda <sergio4234@gmail.com>
2017-03-20 21:57:43 -0500

    Add geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -19,6 +19,18 @@
        console.log("New user joined: ", user)
      });
 
+     socket.on("newLocationMessage", function(message) {
+       var li = jQuery("<li></li>");
+       var a = jQuery('<a target="_blank">My current location</a>');
+
+       li.text(`${message.from}: `);
+       a.attr('href', message.url);
+       li.append(a);
+       jQuery("#messages").append(li);
+
+     });
+
+
      jQuery("#message-form").on("submit", function(e) {
          e.preventDefault();
 
@@ -28,3 +40,19 @@
          }, function(){
          });
      });
+
+  var locationButton = jQuery("#send-location");
+  locationButton.on("click", function(){
+    if (!navigator.geolocation) {
+       return alert("Geolocation not suppoerted by your browser");
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+      socket.emit("createLocationMessage", {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    }, function() {
+      alert("Unable to fetch location.")
+    })
+  })
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express  = require("express");
 const path     = require("path");
 const http     = require("http");
 const socketIO = require("socket.io");
-const {generateMessage} = require("./utils/message");
+const {generateMessage, generateLocationMessage} = require("./utils/message");
 
 
 const port = process.env.PORT || 3000;
@@ -25,15 +25,13 @@ socket.on("createMessage", (message, callback) => {
   console.log("Server Creating a new message: ", message);
   io.emit("newMessage", generateMessage(message.from, message.text));
   callback("This is from the server");
-
-  // socket.broadcast.emit("newMessage", {
-  //   from: message.from,
-  //   text: message.text,
-  //   createdat: new Date().getTime()
-  // });
-
 });
 
+socket.on("createLocationMessage", (coords) => {
+  io.emit("newLocationMessage", generateLocationMessage("Admin",
+                                                coords.latitude,
+                                                coords.longitude ));
+});
 
   socket.on("disconnect", () => {
   console.log("User disconnected")
